ea9e60f82fd65568656b7c7937e6b5bcad788d2f
Paris Dolfman <parisdolfman@gmail.com>
2018-10-23 20:17:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
 
+class CashRegister
+
+attr_accessor :total, :discount
+
+
+def initialize(discount = 0)
+  @total = total
+  @total = 0
+  @discount = discount
+
+end
+
+def add_item(title, price, quantity = 0)
+  #@title = title
+  #@price = price
+  #@quantity = quantity
+  @total << @price
+end
+
+
+end
