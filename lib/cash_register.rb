fef7c633ed2ae2b6f3a8537f5eed6870436feb4e
Paris Dolfman <parisdolfman@gmail.com>
2018-10-24 19:52:47 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,32 +1,37 @@
 
 class CashRegister
 
-attr_accessor :total, :discount, :items
+attr_accessor :total, :discount, :items #reader & writer methods for each variable
 
 
-def initialize(discount = 0)
-  @total = total
-  @total = 0
+def initialize(discount = 0) #discount set to optional
+  @total = total #setting to instance variables
+  @total = 0  #counter set to 0
   @discount = discount
-  @items = []
+  @items = [] #sets variable to equal empty array
 end
 
 
-def add_item(title, price, quantity = 1)
-  @price = price
-  @total += price * quantity
-  @items << title
+def add_item(title, price, quantity = 1) #three arguments, one optional or auto equal 1 is unused
+  @price = price #setting to instance variable
+  @total += price * quantity #the price times quantity plus the total is equal to @ total
+  @items << title #shovel title onto @items variable
 end
 
-def apply_discount(discount = 0)
+def apply_discount(discount = 0) #discount optional
   if @discount == 0
    "There is no discount to apply."
   else
-    @total -= @total * @discount / 100
-    "After the discount, the total comes to $#{@total}."
+    @total -= @total * @discount / 100 #discount divided by 100 and subtracted by total equals variable @total
+    "After the discount, the total comes to $#{@total}." #string interpolation to return correct amount
   end
 end
 
+def items
+  @items.each do |item_name|
+  @items << item_name
+  end
+end
 
 
 end
